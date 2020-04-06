65713571b93d6ab171641d7d237e69db2ed899d0
khrouatbi <33653833+khrouatbi@users.noreply.github.com>
2020-04-06 17:02:40 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# Game-Of-Life-Terminal-1
+
+To run on command prompt:
+1. cd directory.
+2. npm install
+3. node app.js
+4. go to localhost:3000
+
+Works on Heroku.
+
+
+## Server
+- Node.JS
+- Express
+- socket.io
+
+## files:
+
+- `'/api/users/updateIsTyping'` :`socketId` and a `isTyping`.
+- `'/api/messages/list'` : The messages
+- `'/api/messages/submit'` : `socketId` parameter and a `body` parameter
+- `'/api/messages/clear'` : `socketId`. Clears all messages.
+- `'/api/grid/current'` : the current grid state
+- `'/api/grid/start'` : Starts simulation
+- `'/api/grid/pause'` : Pauses simulation
+- `'/api/server/info'` : info about the server
+- `'/api/users/list'` : connected users
+- `'/api/users/rename'` :`socketId` and a `username`.
+
+## Client
+- Angular
+- Angular's Boostrap UI
+- socket.io
---
+++ shared.js
@@ -0,0 +1,35 @@
+module.exports = {
+    io: {},
+    users: {},
+    simulationId: undefined,
+    grid: {
+        rows: 20,
+        cols: 20,
+        cells: [
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+            [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
+        ]
+    },
+    messages: [{
+        username: 'khalil',
+        body: 'First message ever!'
+    }]
+};
